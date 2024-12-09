3e0580b4710a77565303b7a4492fbe4fe125b1f3
Yue <janie.zhaoy@gmail.com>
2024-12-09 06:45:54 -0500

    1

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-document.getElementById('resume-link').addEventListener('click', function() {
-    window.location.href = 'https://drive.google.com/file/d/1ilJF1MGTWqwiwIhHVm0ZhKF2kUKFxlQo/view?usp=sharing';
-});
+function navigateTo(page) {
+    if (page === 'resume-link') {
+        window.location.href = 'https://drive.google.com/file/d/1ilJF1MGTWqwiwIhHVm0ZhKF2kUKFxlQo/view?usp=sharing'; 
+    } else {
+        window.location.href = page;
+    }
+}
 
 function toggleMenu() {
     var menu = document.querySelector('.mobile-menu');
