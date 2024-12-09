1d115e92bda9d267e2e38f0a7bc0b1e93c6cfe03
Yue <janie.zhaoy@gmail.com>
2024-12-09 05:42:33 -0500

    update

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 document.getElementById('resume-link').addEventListener('click', function() {
     window.location.href = 'https://drive.google.com/file/d/1ilJF1MGTWqwiwIhHVm0ZhKF2kUKFxlQo/view?usp=sharing';
 });
+
+function toggleMenu() {
+    var menu = document.querySelector('.mobile-menu');
+    if (menu.style.display === 'flex') {
+        menu.style.display = 'none';
+    } else {
+        menu.style.display = 'flex';
+    }
+}
